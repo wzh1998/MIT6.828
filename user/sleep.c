85c1efadf592366122eeaae7972af83f0705730d
Zihao Wang <zihao.wang20@imperial.ac.uk>
2022-01-31 11:35:11 +0800

    Task <Sleep> completed

---
+++ user/sleep.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+	if(argc != 2) {
+		printf("Usage: sleep <time>\n");
+		exit(-1);
+	}
+	int period = atoi(argv[1]);
+	if(period <= 0) {
+		printf("Invalid period, has to be bigger than 0\n");
+		exit(-2);
+	}
+
+	sleep(period);
+
+	exit(0);
+}
