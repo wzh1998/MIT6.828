aa0b41b02352c01f4084ae5bd67d77525e8c8583
Zihao Wang <zihao.wang20@imperial.ac.uk>
2022-02-02 08:23:02 +0800

    Commented find.c

--- user/find.c
+++ user/find.c
@@ -6,12 +6,11 @@
 char* fmtname_no_padding(char *path) {
 	char *p;
 
-	// Find first character after last slash.
+	// p指向全局路径最后一个'/'的后一位，即文件名的第一位
 	for(p=path+strlen(path); p >= path && *p != '/'; p--)
 	;
 	p++;
 
-	// Return no-padding name.
 	return p;
 }
 
@@ -30,35 +29,29 @@ void find(char *path, char *target_fn) {
 		close(fd);
 		return;
 	}
-	// printf("st.type = %d\n", st.type);
-	// printf("T_DIR = %d\n", T_DIR);
+
 	switch(st.type){
-		// 类型为文件：检查文件名是否匹配
-		
+		// path为文件：检查文件名是否匹配
 		case T_FILE:
 			// 目标文件名匹配
-			// printf("file: %s\t", fmtname_no_padding(path));
-			// printf("compare: %d\n", strcmp(fmtname_no_padding(path), target_fn));
 			if(strcmp(fmtname_no_padding(path), target_fn) == 0)
 				printf("%s\n", path);
 			break;
 
-		// 类型为路径：Recursion
+		// path为路径：递归检查其下所有文件/文件夹
 		case T_DIR:
-			// 遍历该路径下所有的文件及文件夹以寻找目标文件
+			// 遍历该路径下所有的文件/文件夹以寻找目标文件
 			while(read(fd, &de, sizeof(de)) == sizeof(de)){
+				// 注意：'.'及'..'包含在当前路径，需排除以避免无限递归
 				if(de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0) 
 					continue;
 				// 组装该子目录下所有文件及文件夹的全局路径
 				char *new_path = malloc(strlen(path) + 1 + strlen(de.name));
-				// printf("new len: %d\n", strlen(path) + 1 + strlen(de.name));
-				// printf("de.name: %s\n", de.name);
 				strcpy(new_path, path);
 				strcpy(new_path + strlen(path), "/");
 				strcpy(new_path + strlen(path) + 1, de.name);
-				// printf("%s\n", new_path);
-				// exit(-2);
-				// recursion
+
+				// 递归
 				find(new_path, target_fn);
 			}
 			break;
@@ -72,12 +65,10 @@ int main(int argc, char *argv[]) {
 		printf("Usage: find <dir> <fn>");
 		exit(-1);
 	}
-	// printf("argv 0: %s\n", argv[0]);
-	// printf("argv 1: %s\n", argv[1]);
-	// printf("argv 2: %s\n", argv[2]);
-	// int i;
-	// for(i = 2; i < argc; i++) 
-	find(argv[1], argv[2]);
+
+	int i = 0;
+	for(i = 2; i < argc; i++) 
+		find(argv[1], argv[i]);
 	
 	exit(0);
 }
